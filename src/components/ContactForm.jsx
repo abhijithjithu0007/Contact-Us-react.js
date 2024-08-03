f65cae5262f6c7c48fa6c4012d7963542cdc0b66
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-08-03 16:41:34 +0530

    started validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,26 +1,42 @@
 import React, { useState } from 'react';
 import './Styles.scss';
-import { MdDarkMode,MdOutlineLightMode  } from "react-icons/md";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 
 const ContactForm = () => {
 
-  const [mode ,setMode] = useState(false)
-  const handleMode=()=>{
-    setMode((prevState)=>!prevState)
+  const [mode, setMode] = useState(false)
+  const [send, setSend] = useState(false)
+  const [data, setData] = useState({ name: '', email: '' })
+
+
+
+  const handleMode = () => {
+    setMode((prevState) => !prevState)
   }
+  const handleSend = () => {
+    setSend(true)
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setData({ ...data, [name]: value })
+  }
+
+console.log(data);
+
   return (
     <div className="background">
       <div className="container">
-        <div className={mode?"screen":"screen-dark"}>
-          <div className={mode?"screen-header":"screen-header-dark"}>
+        <div className={mode ? "screen" : "screen-dark"}>
+          <div className={mode ? "screen-header" : "screen-header-dark"}>
             <div className="screen-header-left">
               <div className="screen-header-button close"></div>
               <div className="screen-header-button maximize"></div>
               <div className="screen-header-button minimize"></div>
             </div>
-            <div onClick={handleMode} className={mode?'mode-white':'mode-dark'}>
-              {mode?<MdOutlineLightMode />:<MdDarkMode />}
+            <div onClick={handleMode} className={mode ? 'mode-white' : 'mode-dark'}>
+              {mode ? <MdOutlineLightMode /> : <MdDarkMode />}
             </div>
           </div>
           <div className="screen-body">
@@ -34,24 +50,24 @@ const ContactForm = () => {
             <div className="screen-body-item">
               <div className="app-form">
                 <div className="app-form-group">
-                  <input className="app-form-control" placeholder="NAME"/>
+                  <input onChange={(e) => handleChange(e)} className="app-form-control" placeholder="NAME" />
                 </div>
                 <div className="app-form-group">
-                  <input className="app-form-control" placeholder="EMAIL" />
-                </div>
-                <div className="app-form-group">
-                  <input className="app-form-control" placeholder="CONTACT NO" />
+                  <input onChange={(e) => handleChange(e)} className="app-form-control" placeholder="EMAIL" />
                 </div>
                 <div className="app-form-group message">
                   <input className="app-form-control" placeholder="MESSAGE" />
                 </div>
                 <div className="app-form-group buttons">
                   <button className="app-form-button">CANCEL</button>
-                  <button className="app-form-button">SEND</button>
+                  <button onClick={handleSend} className="app-form-button">SEND</button>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+        <div>
+          {send ? (<p>Please fill it</p>) : ''}
         </div>
       </div>
     </div>
